8b4520a2d56d06ca6e213468f0dade136da3dc59
Philip Cristiano <philipcristiano@gmail.com>
2013-01-17 08:41:52 -0500

    v0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 def run_setup():
     setup(
         name='amqp-dispatcher',
-        version='0.0.3',
+        version='0.0.4',
         description='A daemon gevent to run AMQP consumers',
         keywords = 'amqp',
         url='http://github.com/philipcristiano/amqp-dispatcher',
