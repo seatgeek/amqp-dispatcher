69bb4cb88f9b88b553fa7bc52f3c9e1d85f79520
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-09-12 14:48:14 -0400

    Use print instead of logger for error messages
    The python logger may be somehow reconfigured by a worker during it's import tests

--- amqpdispatcher/validate.py
+++ amqpdispatcher/validate.py
@@ -12,14 +12,14 @@ def validate(config_file):
     logger = logging.getLogger('amqp-dispatcher')
     logger.setLevel(logging.ERROR)
     config = yaml.safe_load(open(config_file).read())
-    valid_handler = validate_startup_handler(config, logger)
-    valid_consumers = validate_consumers(config, logger)
+    valid_handler = validate_startup_handler(config)
+    valid_consumers = validate_consumers(config)
     if valid_handler and valid_consumers:
         return
     sys.exit(1)
 
 
-def validate_startup_handler(config, logger):
+def validate_startup_handler(config):
     startup_handler_str = config.get('startup_handler')
     if startup_handler_str is None:
         return True
@@ -28,20 +28,20 @@ def validate_startup_handler(config, logger):
     try:
         handler = load_module_object(startup_handler_str)
     except ImportError, e:
-        logger.error('[{0}] Invalid startup_handler: {1}'.format(
+        print('[{0}] Invalid startup_handler: {1}'.format(
             startup_handler_str,
             e
         ))
         return False
     except AttributeError, e:
-        logger.error('[{0}] Invalid startup_handler: {1}'.format(
+        print('[{0}] Invalid startup_handler: {1}'.format(
             startup_handler_str,
             e
         ))
         return False
 
     if handler is None:
-        logger.error('[{0}] Invalid startup_handler'.format(
+        print('[{0}] Invalid startup_handler'.format(
             startup_handler_str
         ))
         return False
@@ -49,38 +49,38 @@ def validate_startup_handler(config, logger):
     return True
 
 
-def validate_consumers(config, logger):
+def validate_consumers(config):
     consumers = config.get('consumers', None)
     if not consumers:
-        logger.error('No consumers specified')
+        print('No consumers specified')
         return False
 
     is_valid = True
     for consumer in config.get('consumers', []):
-        if not is_consumer_valid(consumer, logger):
+        if not is_consumer_valid(consumer):
             is_valid = False
     return is_valid
 
 
-def is_consumer_valid(consumer, logger):
+def is_consumer_valid(consumer):
     is_valid = True
     consumer_str = consumer.get('consumer', None)
     consumer_klass = None
     try:
         consumer_klass = load_consumer(consumer_str)
         if consumer_klass is None:
-            logger.error('[{0}] Invalid consumer class'.format(consumer_str))
+            print('[{0}] Invalid consumer class'.format(consumer_str))
             is_valid = False
     except ImportError, e:
-        logger.error('[{0}] Invalid consumer class: {1}'.format(consumer_str, e))
+        print('[{0}] Invalid consumer class: {1}'.format(consumer_str, e))
         is_valid = False
     except AttributeError, e:
-        logger.error('[{0}] Invalid consumer class: {1}'.format(consumer_str, e))
+        print('[{0}] Invalid consumer class: {1}'.format(consumer_str, e))
         is_valid = False
 
     queue_name = consumer.get('queue', None)
     if not queue_name:
-        logger.error('[{0}] No queue name specified'.format(
+        print('[{0}] No queue name specified'.format(
             consumer_str
         ))
         is_valid = False
@@ -88,7 +88,7 @@ def is_consumer_valid(consumer, logger):
     for key in ['prefetch_count', 'consumer_count']:
         value = consumer.get(key, 1)
         if not isinstance(value, int):
-            logger.error('[{0}] {1} must be an integer, found {2}'.format(
+            print('[{0}] {1} must be an integer, found {2}'.format(
                 consumer_str,
                 key,
                 value
